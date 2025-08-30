1c4102662371568168fbe85389aceac465884d06
Tim Sawyer <tsawyer94til@gmail.com>
2025-08-30 14:02:28 -0500

    corrected push final

---
+++ mod5.js
@@ -0,0 +1,39 @@
+console.log("Wussup Tho World");
+users[1,12,3]
+
+
+
+// Random Song & Random Song Section Function - On the music page when hovering ver a album card, the album begins to play a random song at a random section
+    function randomSongPlayer(album) {
+        let randomalbum = Math.floor(Math.random() * album.duration);
+        let randomsong = album[randomalbum * Math.floor(Math.random(song.id)  * Math.floor(Math.random) * song.duration)];
+        
+        return randomsong
+    }
+// Form Processing Validation Function - Retrieve a user and activate returning user privs.
+    function userList(user)
+        const userlist = users//'[]
+    function getUser(){
+        if (user.name in userList(all))
+            return returnUser()
+    }
+// Recepit Email Delivery
+
+// Purchasing Functions - Functions that create and manage a cart for checkout
+    function shopCart() {
+        const cart = []
+        function addToCart(item) {
+            item.itemid + cart
+        function takeFromCart(item) {
+            item.itemid - cart
+    } 
+    }
+}
+
+    function items() {
+        const item = {
+            itemid: "",
+            itemprice: "",
+            itemzone:"",
+        }
+    }
